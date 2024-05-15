7f8b516e5d26fcce73de7e46e03950255ff7d884
RomanLedenev <pommanbko@mail.ru>
2024-05-15 17:14:34 +0200

    commit1

--- src/main/java/org/example/_2024_05_15/Main.java
+++ src/main/java/org/example/_2024_05_15/Main.java
@@ -8,8 +8,8 @@ public class Main {
         int intValue = 2000000; // (-2147483648 до 2147483647) 4 байта
         long longValue = 5000_000_000L; // (-9 * 10^18 до 9  * 10^18) 8 байт
 
-        float floatValue = 123.123f; // (до 7 десятичных цифр) 4 байта
-        double doubleValue = 123.123; // (до 16 десятичных цифр) 8 байта
+        float floatValue = 123.45678f; // (до 8 десятичных цифр) 4 байта
+        double doubleValue = 123.4567812345678; // (до 16 десятичных цифр) 8 байта
 
         System.out.println(floatValue);
         System.out.println(doubleValue);
